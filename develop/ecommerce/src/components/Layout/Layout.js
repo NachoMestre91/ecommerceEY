4d8539e413ec3fc0c867b651469eca836d084d15
Nacho Mestre <mestrenacho1991@gmail.com>
2022-09-25 19:02:06 -0300

    Import componentes en layout prueba

--- develop/ecommerce/src/components/Layout/Layout.js
+++ develop/ecommerce/src/components/Layout/Layout.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Header from '../Header/Header.jsx';
 import Footer from '../Footer/Footer.jsx';
-import banner from '../../components/UI/Slider/banner.jsx';
+
 import Home from '../../pages/Home.jsx';
+import db from '../../firebase/firebaseConfig';
+import {collection, getDocs, getFirestore} from 'firebase/firestore';
+// import Slider from '../UI/Slider/Slider';
 
 const Layout = () => {
+  // useEffect(() => {
+  //   const obtenerDatos = async () => {
+  //     const data = await getDocs(collection(db, 'productos'));
+  //     console.log(data);
+  //   };
+  //   obtenerDatos();
+  // }, []);
+
   return (
     <div>
       <Header />
       <Home></Home>
-      <h1>Layout productos</h1>
+      <p>FIREBASE TESTs</p>
+      {/* <Slider /> */}
       <Footer />
     </div>
   );
