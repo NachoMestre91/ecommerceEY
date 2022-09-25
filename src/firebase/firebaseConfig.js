04636e0b58f5bcf979800201180a6016e11d0431
Nacho Mestre <mestrenacho1991@gmail.com>
2022-09-25 19:02:57 -0300

    Imploy firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+
+import {initializeApp} from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDiGPQqzS4oCN75ojPquD43K1Zh8LVJVto',
+  authDomain: 'ecommerce-ey.firebaseapp.com',
+  projectId: 'ecommerce-ey',
+  storageBucket: 'ecommerce-ey.appspot.com',
+  messagingSenderId: '711964670543',
+  appId: '1:711964670543:web:6734cce96540ba7d66883a',
+};
+
+const app = initializeApp(firebaseConfig);
