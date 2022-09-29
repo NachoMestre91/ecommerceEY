c2d10e0760a6b90d99a0d5ac28d16d921f51df45
Nacho Mestre <mestrenacho1991@gmail.com>
2022-09-29 09:30:49 -0300

    Conexion database firebase

--- develop/ecommerce/src/components/Layout/Layout.js
+++ develop/ecommerce/src/components/Layout/Layout.js
@@ -7,28 +7,47 @@ import Carts from '../../components/UI/Cart/Carts.jsx';
 import ProductCard from '../../components/UI/ProductCard/ProductCard.jsx';
 import Products from '../../pages/Products';
 import Home from '../../pages/Home';
-
-// import db from '../../firebase/firebaseConfig';
+import {collection, getDocs, addDoc, updateDoc, deleteDoc, doc} from 'firebase/firestore';
+import {db} from '../../firebase/firebaseConfig.js';
 // import {collection, getDocs, getFirestore} from 'firebase/firestore';
 // import Slider from '../UI/Slider/Slider';
 
 const Layout = () => {
-  // useEffect(() => {
-  //   const obtenerDatos = async () => {
-  //     const data = await getDocs(collection(db, 'productos'));
-  //     console.log(data);
-  //   };
-  //   obtenerDatos();
-  // }, []);
+  const [productos, setProductos] = useState([]);
+  const usersCollectionRef = collection(db, 'productos');
+
+  useEffect(() => {
+    const getProductos = async () => {
+      const data = await getDocs(usersCollectionRef);
+      console.log(data);
+      setProductos(data.docs.map(doc => ({...doc.data(), id: doc.id})));
+    };
+
+    getProductos();
+  }, []);
 
   return (
     <div>
       <Header />
+      {productos.map(products => {
+        return (
+          <div>
+            {' '}
+            <p>ID: {products.id}</p>
+            <p>titulo: {products.title}</p>
+            <p>imagen: {products.image}</p>
+            <p>descripcion larga: {products.largeDescription}</p>
+            <p>Precio: {products.price}</p>
+            <p>Descripcion corta: {products.shortDescription}</p>
+            <p>stock: {products.stock}</p>
+          </div>
+        );
+      })}
 
       <div>
         <Routers />
       </div>
-      <Home />
+
       <Cart />
       <Products />
       {/* <Carts /> */}
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../Header/Header.jsx';
 import Footer from '../Footer/Footer.jsx';
 import Routes from '../../routes/Routers';
 import Carts from '../UI/cart/Carts.jsx';
 import {useSelector} from 'react-redux';
+import {collection, getDocs, addDoc, updateDoc, deleteDoc, doc} from 'firebase/firestore';
+import {db} from '../../firebase/firebaseConfig';
 
 const Layout = () => {
+  const [productos, setProductos] = useState([]);
+
+  const usersCollectionRef = collection(db, 'productos');
+
+  useEffect(() => {
+    const getProductos = async () => {
+      const data = await getDocs(usersCollectionRef);
+      console.log(data);
+
+      setProductos(data.docs.map(doc => ({...doc.data(), id: doc.id})));
+    };
+
+    getProductos();
+  }, []);
+
+  useEffect(() => {
+    const getProductos = async () => {
+      const data = await getDocs(usersCollectionRef);
+      console.log(data);
+      setProductos(data.docs.map(doc => ({...doc.data(), id: doc.id})));
+    };
+
+    getProductos();
+  }, []);
+
   /* ------ Construccion de Layout  ------ */
 
   const showCart = useSelector(state => state.cartUi.cartIsVisible);
+
   return (
     <div>
       <Header />
+      {productos.map(product => {
+        console.log(product);
+        return <h1 style={{color: 'black'}}>titulo: {product.titulo}</h1>;
+      })}
+      <p style={{color: 'black'}}> Respuesta firabase</p>
       {showCart && <Carts />}
       <div>
         <Routes />
