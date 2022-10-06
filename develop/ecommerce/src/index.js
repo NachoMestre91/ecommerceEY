df49dbfd95ed150b06dd93bcfbe5da5a788cb82f
Nacho Mestre <mestrenacho1991@gmail.com>
2022-10-06 16:05:43 -0300

    provider index

--- develop/ecommerce/src/index.js
+++ develop/ecommerce/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import store from './store/store';
+import {Provider} from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -17,9 +19,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      {/* <Provider store={store}> */}
-      <App />
-      {/* </Provider> */}
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>
 );
