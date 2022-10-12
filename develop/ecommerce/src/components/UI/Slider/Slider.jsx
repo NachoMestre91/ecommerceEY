99b1610a13145ede0d02e6de3f1335ed9c9cfdce
Nacho Mestre <mestrenacho1991@gmail.com>
2022-10-12 17:27:40 -0300

    Componente slider

--- develop/ecommerce/src/components/UI/Slider/Slider.jsx
+++ develop/ecommerce/src/components/UI/Slider/Slider.jsx
@@ -1,29 +1,128 @@
 import React from 'react';
-import {Navigation, Scrollbar, A11y} from 'swiper';
+import {Pagination, Navigation} from 'swiper';
 import {Swiper, SwiperSlide} from 'swiper/react';
-import SliderItem from './SliderItem';
-import 'swiper/scss';
-import 'swiper/scss/navigation';
-import 'swiper/scss/pagination';
 
-const Slider = props => {
-  const {data} = props;
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+// import SliderItem from './SliderItem';
+import Img1 from '../../../assets/images/curso-01.jpg';
+// import Img2 from '../../../assets/images/curso-02.jpg';
+
+const Slider = () => {
+  // const {data} = props;
+
+  const data = [
+    {
+      id: '1',
+      title: 'Angular Basicos',
+      stock: '2',
+      img: Img1,
+    },
+    {
+      id: '2',
+      title: 'Angular avanzado',
+      stock: '4',
+      img: Img1,
+    },
+    {
+      id: '3',
+      title: 'React Basicos',
+      stock: '2',
+      img: Img1,
+    },
+    {
+      id: '4',
+      title: 'Node avanzado',
+      stock: '21',
+      img: Img1,
+    },
+    {
+      id: '5',
+      title: 'Angular intermedio',
+      stock: '28',
+      img: Img1,
+    },
+    {
+      id: '6',
+      title: 'Java',
+      stock: '41',
+      img: Img1,
+    },
+    {
+      id: '7',
+      title: 'Aspx',
+      stock: '8',
+      img: Img1,
+    },
+    {
+      id: '8',
+      title: 'C#',
+      stock: '5',
+      img: Img1,
+    },
+    {
+      id: '9',
+      title: 'Next JS',
+      stock: '17',
+      img: Img1,
+    },
+    {
+      id: '10',
+      title: 'Dart 3',
+      stock: '4',
+      img: Img1,
+    },
+  ];
+
+  const Limitado = 'Limitado';
+  const Disponible = 'Disponible';
+
+  const filtroProductos = data.filter(product => product.stock > 0).slice(0, 9);
 
   return (
     <>
       <section className="slider">
         <Swiper
-          modules={[Navigation, Scrollbar, A11y]}
-          spaceBetween={0}
-          slidesPerView={1}
-          navigation
-          scrollbar={{draggable: true}}
+          slidesPerView={4}
+          spaceBetween={4}
+          slidesPerGroup={3}
+          loop={true}
+          loopFillGroupWithBlank={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
         >
-          {data.slice(1, 4).map(item => (
-            <SwiperSlide key={item.id}>
-              <SliderItem item={item} />
-            </SwiperSlide>
-          ))}
+          {/* {data.slice(1, 10).map(item => ( */}
+          {/* <SwiperSlide key={item.id}> */}
+          <SwiperSlide key={data.id}>
+            <div className="">
+              {filtroProductos.map((data, index) => (
+                <div key={index}>
+                  {index === 0 ? (
+                    <>
+                      <img src={''} alt="" />
+                    </>
+                  ) : (
+                    <>
+                      <div className="">
+                        <img className="" src={data.img} alt=""></img>
+                        <h6 style={{color: '#fcd202'}}>{data.title}</h6>
+
+                        <h6 className="addTOCart__btn" style={{color: '#21242d'}}>
+                          {data.stock <= 5 ? Limitado : Disponible}
+                        </h6>
+                      </div>
+                    </>
+                  )}
+                </div>
+              ))}
+            </div>
+          </SwiperSlide>
+          {/* ))} */}
         </Swiper>
       </section>
     </>
---
+++ src/components/UI/slider/sliderItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const sliderItem = props => {
+
+    const {item} = props;
+
+
+  return (
+    <div><img src={}></img>
+    <h6></h6>
+    </div>
+  )
+}
+
+export default sliderItem
