b967093121325394c2da77940a291599cd79672f
Nacho Mestre <mestrenacho1991@gmail.com>
2022-10-17 12:43:30 -0300

    Componente Slider (conexion firebase y logica disponibilidad

--- develop/ecommerce/src/components/UI/Slider/Slider.jsx
+++ develop/ecommerce/src/components/UI/Slider/Slider.jsx
@@ -1,93 +1,26 @@
 import React from 'react';
 import {Pagination, Navigation} from 'swiper';
 import {Swiper, SwiperSlide} from 'swiper/react';
-
+import {useSelector} from 'react-redux';
+import SliderItem from './SliderItem';
+import './slider.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-// import SliderItem from './SliderItem';
-import Img1 from '../../../assets/images/curso-01.jpg';
-// import Img2 from '../../../assets/images/curso-02.jpg';
 
 const Slider = () => {
-  // const {data} = props;
+  const {products} = useSelector(state => state.product);
 
-  const data = [
-    {
-      id: '1',
-      title: 'Angular Basicos',
-      stock: '2',
-      img: Img1,
-    },
-    {
-      id: '2',
-      title: 'Angular avanzado',
-      stock: '4',
-      img: Img1,
-    },
-    {
-      id: '3',
-      title: 'React Basicos',
-      stock: '2',
-      img: Img1,
-    },
-    {
-      id: '4',
-      title: 'Node avanzado',
-      stock: '21',
-      img: Img1,
-    },
-    {
-      id: '5',
-      title: 'Angular intermedio',
-      stock: '28',
-      img: Img1,
-    },
-    {
-      id: '6',
-      title: 'Java',
-      stock: '41',
-      img: Img1,
-    },
-    {
-      id: '7',
-      title: 'Aspx',
-      stock: '8',
-      img: Img1,
-    },
-    {
-      id: '8',
-      title: 'C#',
-      stock: '5',
-      img: Img1,
-    },
-    {
-      id: '9',
-      title: 'Next JS',
-      stock: '17',
-      img: Img1,
-    },
-    {
-      id: '10',
-      title: 'Dart 3',
-      stock: '4',
-      img: Img1,
-    },
-  ];
-
-  const Limitado = 'Limitado';
-  const Disponible = 'Disponible';
-
-  const filtroProductos = data.filter(product => product.stock > 0).slice(0, 9);
+  const filtroProductos = products.filter(product => product.stock > 0).slice(0, 9);
 
   return (
     <>
-      <section className="slider">
+      <div className="Contenedor-slider-item">
         <Swiper
           slidesPerView={4}
           spaceBetween={4}
-          slidesPerGroup={3}
+          slidesPerGroup={2}
           loop={true}
           loopFillGroupWithBlank={true}
           pagination={{
@@ -96,35 +29,13 @@ const Slider = () => {
           navigation={true}
           modules={[Pagination, Navigation]}
         >
-          {/* {data.slice(1, 10).map(item => ( */}
-          {/* <SwiperSlide key={item.id}> */}
-          <SwiperSlide key={data.id}>
-            <div className="">
-              {filtroProductos.map((data, index) => (
-                <div key={index}>
-                  {index === 0 ? (
-                    <>
-                      <img src={''} alt="" />
-                    </>
-                  ) : (
-                    <>
-                      <div className="">
-                        <img className="" src={data.img} alt=""></img>
-                        <h6 style={{color: '#fcd202'}}>{data.title}</h6>
-
-                        <h6 className="addTOCart__btn" style={{color: '#21242d'}}>
-                          {data.stock <= 5 ? Limitado : Disponible}
-                        </h6>
-                      </div>
-                    </>
-                  )}
-                </div>
-              ))}
-            </div>
+          <SwiperSlide className="slider-item ">
+            {filtroProductos.map((product, item) => (
+              <SliderItem key={item} product={product} />
+            ))}
           </SwiperSlide>
-          {/* ))} */}
         </Swiper>
-      </section>
+      </div>
     </>
   );
 };
--- develop/ecommerce/src/components/UI/Slider/SliderItem.jsx
+++ develop/ecommerce/src/components/UI/Slider/SliderItem.jsx
@@ -1,52 +1,26 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import React from 'react';
 import './SliderItem.css';
 
-const BannerItem = props => {
-  const {item} = props;
-  const [isOpen, setOpen] = useState(false);
+const SliderItem = ({product}) => {
+  const Limitado = 'Limitado';
+  const Disponible = 'Disponible';
+
   return (
-    <div>
-      <div className={`box-slider ${item.classAction}`}>
-        <img className="bg-slider" src={item.bgImg} />
-        <div className="box-slider__main">
-          <div className="container">
-            <div className="row">
-              <div className="col-xl-6 col-md-12">
-                <div className="content-box">
-                  <h1 className="title">{item.title}</h1>
-                  <p className="sub-title">{item.desc}</p>
-                  <div className="wrap-btn">
-                    <Link
-                      to="#"
-                      className="tf-button-st2 btn-effect"
-                      data-toggle="modal"
-                      data-target="#popup_bid"
-                    >
-                      <span className="effect">Inscribirme</span>
-                    </Link>
-                    <button
-                      to="#"
-                      className="tf-button btn-effect popup-youtube"
-                      onClick={() => setOpen(true)}
-                    >
-                      <span className="boder-fade"></span>
-                      <span className="effect">ver video</span>
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className="col-xl-6 col-md-12">
-                <div className="image">
-                  <img src={item.img} />
-                </div>
-              </div>
-            </div>
-          </div>
+    <>
+      <div className="sliderItem product__item">
+        <div>
+          <img className="img-slider" src={product.image} alt=""></img>
+          <h6 style={{color: '#fcd202'}}>{product.title}</h6>
+          <h6 style={{color: '#fcd202'}}>stock: {product.stock}</h6>
+          {product.stock <= 5 ? (
+            <h6 className="stock-limitado">{Limitado}</h6>
+          ) : (
+            <h6 className="stock-disponible">{Disponible}</h6>
+          )}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default BannerItem;
+export default SliderItem;
