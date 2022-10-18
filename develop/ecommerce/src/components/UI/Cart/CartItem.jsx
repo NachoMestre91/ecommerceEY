8b028616da2500240a5a5bed8b5cc0db47632041
Nacho Mestre <mestrenacho1991@gmail.com>
2022-10-18 11:47:21 -0300

    State en carrito

--- develop/ecommerce/src/components/UI/Cart/CartItem.jsx
+++ develop/ecommerce/src/components/UI/Cart/CartItem.jsx
@@ -1,33 +1,68 @@
 import React from 'react';
 import {ListGroupItem} from 'reactstrap';
+import {useDispatch} from 'react-redux';
+import {cartActions} from '../../../store/Slices/cartSlice.js';
 import './CartItem.css';
 
-const CartItem = () => {
+const CartItem = ({item}) => {
+  console.log(item);
+
+  const dispatch = useDispatch();
+
+  const {id, title, price, image, quantity, totalPrice, largeDesciption, shortDescription} = item;
+
+  /* ------ Agregar cantidad Item ------ */
+
+  const incrementItem = () => {
+    dispatch(
+      cartActions.addItem({
+        id,
+        title,
+        price,
+        image,
+        largeDesciption,
+        shortDescription,
+      })
+    );
+  };
+
+  /* ------ sacar cantidad Item  ------ */
+
+  const decreaseItem = () => {
+    dispatch(cartActions.removeItem(id));
+  };
+
+  /* ------ eliminar Item ------ */
+
+  const deleteItem = () => {
+    dispatch(cartActions.deleteItem(id));
+  };
+
   return (
     <>
       <div className="Contenedor-items">
         <ListGroupItem className="border-0 cart__item">
           <div className="cart__item-info d-flex gap-4 flex-row">
-            <img alt="product-img" />
+            <img src={image} alt="product-img" />
 
             <div className="cart__product-info w-100 d-flex align-items-center gap-4 justify-content-between ">
               <div>
                 <h6 className="cart__product-title">Nombre producto</h6>
                 <p className=" d-flex align-items-center gap-5 cart__product-price">
-                  cantidad <span>precio</span>
+                  {quantity}x <span>${totalPrice}</span>
                 </p>
                 <div className=" d-flex align-items-center justify-content-between increase__decrease-btn">
-                  <span className="increase__btn">
+                  <span className="increase__btn" onClick={incrementItem}>
                     <i className="ri-add-line"></i>
                   </span>
-                  <span className="quantity">Cantidad</span>
-                  <span className="decrease__btn">
+                  <span className="quantity">{quantity}</span>
+                  <span className="decrease__btn" onClick={decreaseItem}>
                     <i className="ri-subtract-line"></i>
                   </span>
                 </div>
               </div>
 
-              <span className="delete__btn">
+              <span className="delete__btn" onClick={deleteItem}>
                 <i className="ri-close-circle-line "></i>
               </span>
             </div>
