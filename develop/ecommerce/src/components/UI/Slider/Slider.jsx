447e02febc4c2fe131e8927fba98703d14d47bd4
Nacho Mestre <mestrenacho1991@gmail.com>
2022-10-20 15:04:18 -0300

    mobile Slicer

--- develop/ecommerce/src/components/UI/Slider/Slider.jsx
+++ develop/ecommerce/src/components/UI/Slider/Slider.jsx
@@ -15,45 +15,87 @@ const Slider = () => {
   const Limitado = 'Limitado';
   const Disponible = 'Disponible';
 
+  const userAgent = navigator.userAgent;
+  const isMobileIPhone = userAgent.indexOf('iPhone');
+  const isMobileAndroid = userAgent.indexOf('Android');
+
+  console.log('android', isMobileAndroid);
+  console.log('iphone', isMobileIPhone);
+
   return (
     <>
-      <div className="Contenedor-slider-item">
-        <Swiper
-          modules={[Pagination, Navigation, Autoplay]}
-          slidesPerView={4}
-          spaceBetween={4}
-          slidesPerGroup={2}
-          loop={true}
-          breakpoints={{
-            0: {slidesPerview: 1, spaceBetween: 10},
-            480: {slidesPerview: 2, spaceBetween: 10},
-            768: {slidesPerview: 3, spaceBetween: 15},
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: true,
-          }}
-        >
-          {products.map(product => (
-            <SwiperSlide key={product.id}>
-              <div className="slider-item  product__item filter-drop">
-                <img className="img-slider" src={product.image} alt=""></img>
-                <h6 style={{color: '#fcd202'}}>{product.title}</h6>
-                <h6 style={{color: '#fcd202'}}>stock: {product.stock}</h6>
-                {product.stock <= 5 ? (
-                  <h6 className="stock-limitado">{Limitado}</h6>
-                ) : (
-                  <h6 className="stock-disponible">{Disponible}</h6>
-                )}
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+      {isMobileAndroid !== -1 || isMobileIPhone !== -1 ? (
+        <div className="Contenedor-slider-item">
+          <Swiper
+            modules={[Pagination, Navigation, Autoplay]}
+            slidesPerView={1}
+            spaceBetween={4}
+            slidesPerGroup={2}
+            loop={true}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: true,
+            }}
+          >
+            {products.map(product => (
+              <SwiperSlide key={product.id}>
+                <div className="slider-item  product__item align-items-center filter-drop">
+                  <img className="img-slider" src={product.image} alt=""></img>
+                  <h6 style={{color: '#fcd202'}}>{product.title}</h6>
+                  <h6 style={{color: '#fcd202'}}>stock: {product.stock}</h6>
+                  {product.stock <= 5 ? (
+                    <h6 className="stock-limitado">{Limitado}</h6>
+                  ) : (
+                    <h6 className="stock-disponible">{Disponible}</h6>
+                  )}
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      ) : (
+        <div className="Contenedor-slider-item">
+          <Swiper
+            modules={[Pagination, Navigation, Autoplay]}
+            slidesPerView={4}
+            spaceBetween={4}
+            slidesPerGroup={2}
+            loop={true}
+            // breakpoints={{
+            //   0: {slidesPerview: 1, spaceBetween: 10},
+            //   480: {slidesPerview: 2, spaceBetween: 10},
+            //   768: {slidesPerview: 3, spaceBetween: 15},
+            // }}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: true,
+            }}
+          >
+            {products.map(product => (
+              <SwiperSlide key={product.id}>
+                <div className="slider-item  product__item filter-drop">
+                  <img className="img-slider" src={product.image} alt=""></img>
+                  <h6 style={{color: '#fcd202'}}>{product.title}</h6>
+                  <h6 style={{color: '#fcd202'}}>stock: {product.stock}</h6>
+                  {product.stock <= 5 ? (
+                    <h6 className="stock-limitado">{Limitado}</h6>
+                  ) : (
+                    <h6 className="stock-disponible">{Disponible}</h6>
+                  )}
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      )}
     </>
   );
 };
