d1e400ce85a0e1bf9efb5dcc786270ca52a88363
Nacho Mestre <mestrenacho1991@gmail.com>
2022-10-25 14:04:50 -0300

    Limitacion dispatch en ProductDetail

--- develop/ecommerce/src/pages/ProductDetail.jsx
+++ develop/ecommerce/src/pages/ProductDetail.jsx
@@ -9,50 +9,42 @@ import '../assets/css/ProductDetail.css';
 const ProductDetail = () => {
   const dispatch = useDispatch();
 
+  /* ------ Get States Slice ------ */
+
   const {products} = useSelector(state => state.product);
-
-  /* ------ Inicializacion de estados ------ */
-
-  const [tab, setTab] = useState('desc');
+  const {cartItems} = useSelector(state => state.cart);
 
   /* ------ parametro que trae de detalle ------ */
 
   const {id} = useParams();
 
-  console.log(id);
-
-  /* ------ parametro que trae de detalle ------ */
+  /* ------ Filtro por ID ------ */
 
   const product = products.find(product => product.id === id);
-  console.log(product);
 
   const {title, price, image, stock, largeDescription, shortDescription} = product;
-  const [previewImg, setPreviewImg] = useState(image);
 
-  // console.log({title, price, image, stock, largeDesciption, shortDescription});
-
-  // const relatedProduct = products.filter(item => category === item.category);
+  /* ------ Agregar productos al carrito ------ */
 
   const addItem = () => {
-    dispatch(
-      cartActions.addItem({
-        id,
-        title,
-        price,
-        image,
-      })
-    );
+    const cantidad = cartItems?.find(product => product.id === id);
+
+    const newStock = stock - 1;
+
+    console.log(newStock);
+
+    const cantidaditem = cantidad?.quantity;
+    if (cantidaditem <= newStock || !cantidaditem) {
+      dispatch(
+        cartActions.addItem({
+          id,
+          title,
+          price,
+          image,
+        })
+      );
+    }
   };
-
-  // const submitHandler = e => {
-  //   e.preventDefault();
-
-  //   console.log(enteredName, enteredEmail, reviewMsg);
-  // };
-
-  // useEffect(() => {
-  //   setPreviewImg(product.image);
-  // }, [product]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -65,24 +57,24 @@ const ProductDetail = () => {
           <Row>
             <Col lg="4" md="4">
               <div className="product__main-img product__item">
-                <img src={previewImg} alt="" className="w-50" />
+                <img src={product?.image} alt="" className="w-50" />
               </div>
               <br></br>
               <div className="product_stock ">
-                <h2 className="product__price text-center">Precio: ${price}</h2>
-                <h2 className="product__price text-center">Stock: {stock}</h2>
+                <h2 className="product__price text-center">Precio: ${product?.price}</h2>
+                <h2 className="product__price text-center">Stock: {product?.stock}</h2>
               </div>
               <br></br>
             </Col>
 
             <Col lg="6" md="6">
               <div className="single__product-content">
-                <h2 className="product__title mb-3">{product.title}</h2>
+                <h2 className="product__title mb-3">{product?.title}</h2>
                 <Col lg="12">
-                  <p> {shortDescription}</p>
+                  <p> {product?.shortDescription}</p>
 
                   <h6>Descripcion</h6>
-                  <p>{largeDescription}</p>
+                  <p>{product?.largeDescription}</p>
                 </Col>
               </div>
               <Col lg="12" className="detail-action  cart__page-btn">
